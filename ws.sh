a842000d3b8fd554772e4a0c24f298381f3a77a1
papebsow <papebilalysow@gmail.com>
2023-09-30 04:43:28 +0000

    : set up workspace

--- ws.sh
+++ ws.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+# list of extensons for VSCode
+extensons=(
+    dzhavat.bracket-pair-toggler
+    ecmel.vscode-html-css
+    usernamehw.errorlens
+    fabiospampinato.vscode-todo-plus
+    humao.rest-client
+    liviuschera.noctis
+    mintlify.document
+    PKief.material-icon-theme
+    s-nlf-fh.glassit
+    solnurkarim.html-to-css-autocompletion
+    ritwickdey.LiveServer
+    robertz.code-snapshot
+    Vue.volar
+)
+
+for extenson in "${extensons[@]}"
+do
+    code --install-extension "$extenson"
+done
