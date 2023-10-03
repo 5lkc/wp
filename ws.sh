67119204efb6835b7e17270172d12f952e93f9dc
5lkc <papebilalysow@gmail.com>
2023-10-03 01:14:16 +0000

    : set up workspace environment

--- ws.sh
+++ ws.sh
@@ -9,6 +9,7 @@ extensons=(
     humao.rest-client
     liviuschera.noctis
     mintlify.document
+    ms-vscode.cpptools
     PKief.material-icon-theme
     s-nlf-fh.glassit
     solnurkarim.html-to-css-autocompletion
@@ -17,13 +18,30 @@ extensons=(
     Vue.volar
 )
 
+# Install the list
 for extenson in "${extensons[@]}"
 do
     code --install-extension "$extenson"
 done
 
 # set up terminal appearance
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zakaziko99/agnosterzak-ohmyzsh-theme.git
 mv agnosterzak-ohmyzsh-theme/agnosterzak.zsh-theme .oh-my-zsh/themes
 rm -rf agnosterzak-ohmyzsh-theme
-code .zshrc
+
+# Define the old and new theme settings
+old_theme='ZSH_THEME="robbyrussell"'
+new_theme='ZSH_THEME="agnosterzak"'
+
+# Define the .zshrc file
+zshrc_file="$HOME/.zshrc"
+
+# Check if the .zshrc file exists
+if [ ! -f "$zshrc_file" ]; then
+    echo "Error: .zshrc file not found in your home directory."
+    exit 1
+fi
+
+# Use sed to replace the old theme setting with the new theme setting
+sed -i "s|$old_theme|$new_theme|g" "$zshrc_file"
