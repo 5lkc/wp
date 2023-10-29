f77807be9fe84c292fb23572960212160073016d
5lkc <papebilalysow@gmail.com>
2023-10-29 15:37:04 +0000

    : setup my dev environment

--- setup.sh
+++ setup.sh
@@ -29,31 +29,12 @@ do
 done
 
 # set up terminal appearance . . .
-
-# Install Oh-My-Zsh and set up the "agnosterzak" theme
+# [1]- Install Oh-My-Zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# [2]- Setup the "agnosterzak" theme
 git clone https://github.com/zakaziko99/agnosterzak-ohmyzsh-theme.git
-mv agnosterzak-ohmyzsh-theme/agnosterzak.zsh-theme ../.oh-my-zsh/themes
+mv agnosterzak-ohmyzsh-theme/agnosterzak.zsh-theme .oh-my-zsh/themes
 rm -rf agnosterzak-ohmyzsh-theme
-
-# Define the old and new theme settings
-old_theme='ZSH_THEME="robbyrussell"'
-new_theme='ZSH_THEME="agnosterzak"'
-
-# Define the .zshrc file
-zshrc_file="../.zshrc"
-
-# Check if the .zshrc file exists
-if [ ! -f "$zshrc_file" ]; then
-    echo "Error: .zshrc file not found in your home directory."
-    exit 1
-fi
-
-# Use sed to replace the old theme setting with the new theme setting
-sed -i "s|$old_theme|$new_theme|g" "$zshrc_file"
-
-# Apply the new theme
-exec zsh
 
 # Set my Gitea credencials
 git config --global user.email "papebilalysow@gmail.com"
